99c0e6243c8ff04158309239b6d7e4869e94d42d
Johan Rodriguez <jsrodriguezfajardo@gmail.com>
2020-07-19 14:13:33 -0500

    Adjust user and product models so that all fields are mandatory

--- mongo/models.js
+++ mongo/models.js
@@ -3,17 +3,17 @@ const mongoose = require('./config'),
 
 const schemas = {
     userSchema: new Schema({
-        email: { type: String, unique: true, lowercase: true },
-        displayName: String,
-        password: String
+        email: { type: String, required: true, unique: true, lowercase: true },
+        displayName: { type: String, required: true },
+        password: { type: String, required: true }
     }),
 
     productSchema: new Schema({
-        name: String,
-        picture: String,
-        price: { type: Number, default: 0 },
-        category: { type: String, enum: ['Technology', 'Clothes', 'Home appliances', 'Others', 'Automobiles'] },
-        description: String
+        name: { type: String, required: true },
+        picture: { type: String, required: true },
+        price: { type: Number, required: true },
+        category: { type: String, required: true, enum: ['Technology', 'Clothes', 'Home appliances', 'Others', 'Automobiles'] },
+        description: { type: String, required: true }
     })
 }
 
